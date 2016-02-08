6f43fbfa3893db51e9b9195010a55602c1e5ea8b
gregpiatperso <gregoire.piat@outlook.fr>
2016-02-08 16:45:26 +0100

    Ajout de l'IA récursive

--- src/fr/polytech/tictactoe/game/players/IterativeAIPlayer.java
+++ src/fr/polytech/tictactoe/game/players/IterativeAIPlayer.java
@@ -31,18 +31,232 @@ public class IterativeAIPlayer extends Player
 	@Override
 	public Coordinate getNextChoice(Mark[][] boardGame)
 	{
-		// TODO ADD ALGORITHM
-		for (int x = 0; x < TicTacToe.NB_LINES; x++)
-		{
-			for (int y = 0; y < TicTacToe.NB_COLUMNS; y++)
-			{
-				if (boardGame[x][y] == Mark.EMPTY)
-				{
-					return new Coordinate(x, y);
-				}
-			}
-		}
-
-		return null;
+		boolean found = false;
+        int best_i = 0, best_j = 0;
+        
+        // Vérifie qu'une ligne peut gagner, ou perdre
+        int score;
+        for (int i = 0; i < 3; i++)
+        {
+            score = 0;
+            for (int j = 0; j < 3; j++)
+            {
+            	if(boardGame[i][j] == Mark.O){
+            		score++;
+            	}
+            	else if(boardGame[i][j] == Mark.X){
+            		score--;
+            	}
+            }
+            if (score == -2 || score == 2)
+            {
+                found = true;
+                for (int j = 0; j < 3; j++)
+                {
+                    if (boardGame[i][j] == Mark.EMPTY)
+                    {
+                        best_i = i;
+                        best_j = j;
+                    }
+                }
+            }
+        }
+        // Vérifie qu'une colonne peut gagner, ou perdre
+        for (int j = 0; j < 3; j++)
+        {
+            score = 0;
+            for (int i = 0; i < 3; i++)
+            {
+            	if(boardGame[i][j] == Mark.O){
+            		score++;
+            	}
+            	else if(boardGame[i][j] == Mark.X){
+            		score--;
+            	}
+            }
+            if (score == -2 || score == 2)
+            {
+                found = true;
+                for (int i = 0; i < 3; i++)
+                {
+                    if (boardGame[i][j] == Mark.EMPTY)
+                    {
+                        best_i = i;
+                        best_j = j;
+                    }
+                }
+            }
+        }
+        // Teste la diagonale de gauche à droite
+        score = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            int j = i;
+        	if(boardGame[i][j] == Mark.O){
+        		score++;
+        	}
+        	else if(boardGame[i][j] == Mark.X){
+        		score--;
+        	}
+        }
+        if (score == -2 || score == 2)
+        {
+            found = true;
+            for (int i = 0; i < 3; i++)
+            {
+                int j = i;
+                if (boardGame[i][j] == Mark.EMPTY)
+                {
+                    best_i = i;
+                    best_j = j;
+                }
+            }
+        }
+        // Teste la diagonale de droite à gauche
+        score = 0;
+        int decreaser = 2; // decreaser est ici l'équivalent de j
+        for (int i = 0; i < 3; i++)
+        {
+        	if(boardGame[i][decreaser] == Mark.O){
+        		score++;
+        	}
+        	else if(boardGame[i][decreaser] == Mark.X){
+        		score--;
+        	}
+        	decreaser--;
+        }
+        if (score == -2 || score == 2)
+        {
+            found = true;
+            decreaser = 2;
+            for (int i = 0; i < 3; i++)
+            {
+                if (boardGame[i][decreaser] == Mark.EMPTY)
+                {
+                    best_i = i;
+                    best_j = decreaser;
+                }
+                decreaser--;
+            }
+        }
+        // Choisis la case centrae si vide et pas d'autre meilleur choix
+        if (!found && boardGame[1][1] == Mark.EMPTY)
+        {
+            best_i = 1;
+            best_j = 1;
+        }
+        else if (!found) {
+            // Vérifie les scénarios spécifiques, plus difficiles
+            int nbCasesUsed = nbCasesUsed(boardGame);
+            if (nbCasesUsed == 3)
+            {
+                if (boardGame[0][0] == Mark.O && boardGame[2][2] == Mark.O)
+                {
+                    best_i = 0;
+                    best_j = 1;
+                } 
+                else if (boardGame[0][2] == Mark.O && boardGame[2][0] == Mark.O)
+                {
+                    best_i = 0;
+                    best_j = 1;
+                }
+                else if (boardGame[1][2] == Mark.O && boardGame[2][0] == Mark.O)
+                {
+                    best_i = 2;
+                    best_j = 1;
+                } 
+                else if (boardGame[0][0] == Mark.O && boardGame[1][2] == Mark.O)
+                {
+                    best_i = 0;
+                    best_j = 1;
+                }
+                else if (boardGame[1][0] == Mark.O && boardGame[2][2] == Mark.O)
+                {
+                    best_i = 2;
+                    best_j = 1;
+                }
+                else if (boardGame[0][2] == Mark.O && boardGame[1][0] == Mark.O)
+                {
+                    best_i = 0;
+                    best_j = 1;
+                }
+                else if (boardGame[1][0] == Mark.O && boardGame[2][1] == Mark.O)
+                {
+                    best_i = 2;
+                    best_j = 0;
+                }
+                else if (boardGame[1][2] == Mark.O && boardGame[2][1] == Mark.O)
+                {
+                    best_i = 2;
+                    best_j = 2;
+                }
+                else if (boardGame[1][0] == Mark.O && boardGame[0][1] == Mark.O)
+                {
+                    best_i = 0;
+                    best_j = 0;
+                }
+                else if (boardGame[0][1] == Mark.O && boardGame[1][2] == Mark.O)
+                {
+                    best_i = 0;
+                    best_j = 2;
+                }
+            }
+            else
+            {
+                if (boardGame[0][2] == Mark.EMPTY)
+                {
+                    best_i = 0;
+                    best_j = 2;
+                }
+                else if (boardGame[2][0] == Mark.EMPTY)
+                {
+                    best_i = 2;
+                    best_j = 0;
+                }
+                else if (boardGame[2][2] == Mark.EMPTY)
+                {
+                    best_i = 2;
+                    best_j = 2;
+                }
+                else if (boardGame[0][1] == Mark.EMPTY)
+                {
+                    best_i = 0;
+                    best_j = 1;
+                }
+                else if (boardGame[1][0] == Mark.EMPTY)
+                {
+                    best_i = 1;
+                    best_j = 0;
+                }
+                else if (boardGame[1][2] == Mark.EMPTY)
+                {
+                    best_i = 1;
+                    best_j = 2;
+                }
+                else if (boardGame[2][1] == Mark.EMPTY)
+                {
+                    best_i = 1;
+                    best_j = 1;
+                }
+            }
+        }
+        return new Coordinate(best_i, best_j);
 	}
+	
+    private int nbCasesUsed(Mark[][] boardGame)
+    {
+        int nbCases = 0;
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (boardGame[i][j] == Mark.O || boardGame[i][j] == Mark.X)
+                {
+                    nbCases += 1;
+                }
+            }
+        }
+
+        return nbCases;
+    }
 }
